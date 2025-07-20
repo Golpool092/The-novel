66bf39902f76c87861164342da50cb7dbb83f564
Golpool092 <165432682+Golpool092@users.noreply.github.com>
2025-07-20 13:54:01 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -6,31 +6,6 @@ const urlsToCache = [
   './main.js',
   './story.js',
   './manifest.json',
-  './video/beginning.mp4',
-  './audio/beginning.mp3',
-  './audio/dooroff.mp3',
-  './audio/paper.mp3',
-  './audio/fon.mp3',
-  './pages/diary.png',
-  './pages/diar.png',
-  './pages/pass.png',
-  './pages/passed.png',
-  './pages/settings.png',
-  './pages/fon1.png',
-  './pages/fon2.png',
-  './pages/fon3.png',
-  './pages/fon4.png',
-  './pages/fon5.png',
-  './pages/fon6.png',
-  './pages/fon7.png',
-  './pages/fon8.png',
-  './pages/fon5-1.png',
-  './pages/fon6-1.png',
-  './pages/fon7-1.png',
-  './pages/fon8-1.png',
-  './pages/icon-192.png',
-  './pages/icon-512.png',
-  './pages/dowicon.png'
 ];
 
 self.addEventListener('install', (event) => {
@@ -39,7 +14,7 @@ self.addEventListener('install', (event) => {
       return cache.addAll(urlsToCache);
     })
   );
-  self.skipWaiting(); // активировать SW сразу после установки
+  self.skipWaiting(); 
 });
 
 self.addEventListener('activate', (event) => {
@@ -52,28 +27,30 @@ self.addEventListener('activate', (event) => {
       )
     )
   );
-  self.clients.claim(); // контролировать страницы сразу
+  self.clients.claim(); 
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
-      // Возвращаем кеш если есть
       if (cachedResponse) return cachedResponse;
-      // Иначе делаем запрос в сеть и кешируем ответ
+
       return fetch(event.request).then((networkResponse) => {
-        // Если невалидный ответ, просто возвращаем
-        if(!networkResponse || networkResponse.status !== 200 || networkResponse.type !== 'basic') {
+        if (
+          !networkResponse ||
+          networkResponse.status !== 200 ||
+          networkResponse.type !== 'basic'
+        ) {
           return networkResponse;
         }
-        // Клонируем ответ для кеша
+
         const responseToCache = networkResponse.clone();
         caches.open(CACHE_NAME).then((cache) => {
           cache.put(event.request, responseToCache);
         });
+
         return networkResponse;
       });
     })
   );
 });
-
